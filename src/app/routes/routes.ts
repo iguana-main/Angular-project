df13013e0ecdf6ac9e5ce100f82ba5a8d612626c
Zosia Samosia <Zosia@Samosia.pl>
2026-06-12 11:58:59 +0200

    Basic layout with Angular routing

---
+++ src/app/routes/routes.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Routes } from '@angular/router'; 
+import { Contact } from '../contact/contact';
+
+export const routes: Routes = [
+  {
+    path : '../contact/contact.html',
+    component : Contact, 
+  }
+];
