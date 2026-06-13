d50791d2a60f141312de9091f7112933f5069773
Anna Nowak <anna@nowak.com>
2026-06-13 22:27:57 +0200

    Refactor code structure for improved readability and maintainability

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Offers } from './offers/offers';
 import { AbtUs } from './abt-us/abt-us';
 import { Contact } from "./contact/contact";
 import { Opinions } from "./opinions/opinions";
+import { Footer } from './footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, Offers, AbtUs, Contact, Opinions],
+  imports: [RouterOutlet, Home, Offers, AbtUs, Contact, Opinions, Footer],
   templateUrl: './app.html',
   styleUrl: './app.scss',
   standalone: true,
